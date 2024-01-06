7b5f04927ac5baf15428c6b2e6ba7afb619cc5ed
Anurag Jain <98457054+anuragjain-git@users.noreply.github.com>
2024-01-06 20:27:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,14 +12,16 @@ $(document).ready(function() {
 
         setInterval(function() {
             var age = getAge(dob);
-            $("#age").html(age.year + "<sup>." + age.ms + "</sup>");
+           // $("#age").html(age.year + "<sup>." + age.ms + "</sup>");
+           $("#age").html("<span class='year'>" + age.year + "</span>" +
+           "<sup class='ms'>." + age.ms + "</sup>");
         }, 100);
     }
 
     function getAge(dob) {
         var now = new Date();
-        var duration = now - dob;
-        var years = duration / 31556900000;
+        var duration = now - dob; // time difference in ms
+        var years = duration / 31556900000; // 31556900000 is ms in one year 
         var majorMinor = years.toFixed(9).toString().split('.');
 
         return {
